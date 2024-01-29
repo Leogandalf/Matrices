c568f9148c2d8967b5e2bf7b976082f0ce3408dd
Leogandalf <116030718+Leogandalf@users.noreply.github.com>
2024-01-29 21:54:44 +0100

    Add files via upload

---
+++ matrices.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+class matrix{
+	
+	
+};
+
+class controller{
+	
+	
+	
+};
+
+
+int main(){
+	printf("Insert the number of rows of the matrix...");
+	cin int row;
+	printf("Insert the number of columns of the matrix...");
+	cin int column;
+	
+	if ()
+	
+	cin matrix (A);
+		
+	
+	
+	return 0;
+}
